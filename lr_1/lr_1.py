e4e211e7ba8c8855cadcf1205ae42995d8ca5471
manukyan artur <nowrim@mail.ru>
2023-09-19 20:51:58 +0300

    чтение видео из файла и запись в другой файл

--- lr_1/lr_1.py
+++ lr_1/lr_1.py
@@ -52,3 +52,69 @@ def show_video(path='media/willem dafoe'):
 
 
 show_video()
+"""
+Задание 4 Записать видео из файла в другой файл.
+"""
+
+
+def read_and_write(path='media/willem dafoe.mp4'):
+
+    video_capture = cv2.VideoCapture(path)
+    video_writer = cv2.VideoWriter(
+        'media/output.mp4',
+        0,
+        20.0,
+        (int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)))
+    )
+    while video_capture.isOpened():
+        is_successful, frame = video_capture.read()
+        if is_successful:
+            video_writer.write(frame)
+            cv2.imshow('willem dafoe', frame)
+            key = cv2.waitKey(20)
+            if cv2.waitKey(5) == ord('q') or key == 27:
+                break
+        else:
+            break
+
+    video_capture.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+
+
+read_and_write()
+show_video('media/output.avi')
+
+
+"""
+Задание 5. Прочитать изображение, перевести его в формат HSV.
+Вывести на экран два окна, в одном изображение в формате HSV, в другом –
+исходное изображение.
+"""
+
+
+
+"""
+Задание 6. Прочитать изображение с камеры. Вывести
+в центре на экране Красный крест в формате, как на изображении. Указать
+команды, которые позволяют это сделать. 
+"""
+
+
+"""
+Задание 7. Отобразить информацию с вебкамеры,
+записать видео в файл, продемонстрировать видео."""
+
+"""
+Задание 8. Залить крест одним из 3 цветов – красный,
+зеленый, синий по следующему правилу: НА ОСНОВАНИИ ФОРМАТА RGB 
+определить, центральный пиксель ближе к какому из цветов красный,
+зеленый, синий и таким цветом заполнить крест.
+"""
+
+
+"""
+Подключите телефон, подключитесь к его
+камере, выведете на экран видео с камеры. Продемонстрировать процесс на
+ноутбуке преподавателя и своем телефоне.
+"""
